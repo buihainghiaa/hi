23ff7fec3a7c9b879cbca26e577716a08c9786bb
buihainghiaa <93524625+buihainghiaa@users.noreply.github.com>
2021-12-17 22:48:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Em';
-const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const nameGirl = 'BHT 💕💕';
+const giftUrl = 'https://www.facebook.com/thuong.buihuyen.1212';
+const eventName = 'Hello 👋👋';
+const titleCard = 'BHT nè ';
+const contentCard = 'Đây là một cô bé dễ thươngg 🥰🤗🤪';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
