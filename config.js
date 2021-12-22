c3a2a13d9f3bf68f1c37a1216d69bc631c3483f5
buihainghiaa <93524625+buihainghiaa@users.noreply.github.com>
2021-12-22 15:22:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,10 +4,7 @@ const eventName = 'Hello 👋👋';
 const titleCard = 'BHT nè ';
 const contentCard = 'Đây là một cô bé dễ thươngg 🥰🤗🤪';
 
-// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
-// Cần hỗ trợ hãy liên hệ: 
-// Mr-Nam http://facebook.com/nam.nodemy
-// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+
 const giftImage = 'bht.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
