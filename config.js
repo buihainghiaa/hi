01679e47dd1f42a2e70355f6a9ed2a1e1459ac38
buihainghiaa <93524625+buihainghiaa@users.noreply.github.com>
2021-12-31 21:02:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-const nameGirl = ' 💕💕';
-const giftUrl = 'https://www.facebook.com/dunn236';
+const nameGirl = '😄😄';
+const giftUrl = 'https://www.facebook.com/nguyen.lam.1203';
 const eventName = 'Hello 👋👋';
-const titleCard = 'Dungg nè ';
-const contentCard = 'Duunggu';
+const titleCard = 'Chào bạn nhee 😁';
+const contentCard = 'Cảm ơn bạn trong buổi chiều nay đã tham gia tương tác với nhóm 3 bọn mình để buổi thuyết trình của bọn mình được thành công 🤗 như đã nói thì.. nhóm mình đã cử mình ra chúc bạn 2 tối coi như phần quà dành tặng cho bạn 🌝 um..mong là bạn k chê nhé.. 😅 ';
 
 
-const giftImage = 'ngodung_1.png';
+const giftImage = 'jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
