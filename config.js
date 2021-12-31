7ec135ff571ddb400645d0bb57f504c685ba65b8
buihainghiaa <93524625+buihainghiaa@users.noreply.github.com>
2021-12-31 21:03:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ const titleCard = 'Chào bạn nhee 😁';
 const contentCard = 'Cảm ơn bạn trong buổi chiều nay đã tham gia tương tác với nhóm 3 bọn mình để buổi thuyết trình của bọn mình được thành công 🤗 như đã nói thì.. nhóm mình đã cử mình ra chúc bạn 2 tối coi như phần quà dành tặng cho bạn 🌝 um..mong là bạn k chê nhé.. 😅 ';
 
 
-const giftImage = 'jpg';
+const giftImage = 'ntl.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
